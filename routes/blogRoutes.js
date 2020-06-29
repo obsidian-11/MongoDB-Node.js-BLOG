4e7f7c6f4a25a3a5df946803b74b57fa1b9d1166
devIbrahim <syedaneesibrahom@gmail.com>
2020-06-29 22:57:09 +0530

    Minor code formatting

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const blogController = require('../controllers/blogController');
+const blogController = require("../controllers/blogController");
 
 router.get("/", blogController.blog_index);
 
 router.get("/create", blogController.blog_create_get);
 
-router.post('/create', blogController.blog_create_post);
+router.post("/create", blogController.blog_create_post);
 
-router.delete('/:id', blogController.blog_delete);
+router.delete("/:id", blogController.blog_delete);
 
-router.get(('/:id'), blogController.blog_details);
+router.get("/:id", blogController.blog_details);
 
 module.exports = router;
